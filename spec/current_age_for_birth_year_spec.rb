203eb6388d5569db885f7b36a3d56c522c914b4c
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-27 23:25:48 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,7 @@ require_relative '../current_age_for_birth_year.rb'
 describe "current_age_for_birth_year method" do
   def 
     current_age_for_birth_year(year)
-    return year
+    return #{year}
   end
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
